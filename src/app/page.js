e3cb3439331f7c90e0b36794720ae71173eab20b
Aamin Patel <locomail112@gmail.com>
2025-02-16 23:02:57 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Welcome to My Website!
+      </h1>
+    </main>
+  );
+}
